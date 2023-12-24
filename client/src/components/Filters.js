b67f7efe70f8931ef96ae22bfb35c35be3dee6d4
rd1073 <rd1073@srmist.edu.in>
2023-12-24 20:42:59 +0530

    p

---
+++ client/src/components/Filters.js
@@ -0,0 +1,16 @@
+import { Checkbox, Radio } from "antd";
+
+import React from 'react'
+
+const Filters = () => {
+    const [categories, setCategories] = useState([]);
+  const [checked, setChecked] = useState([]);
+  const [radio, setRadio] = useState([]);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Filters
--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useAuth } from '../context/authContext';
 import { useState, useEffect } from "react";
 import axios from "axios";
+import InputGroup from 'react-bootstrap/InputGroup';
 
 
 const Products = () => {
@@ -12,7 +13,7 @@ const Products = () => {
         try {
         const { data } = await axios.get("http://localhost:5000/all-products");
         setProducts(data.products);
-        console.log(data);
+        //console.log(data);
         } catch (error) {
         console.log(error);
          
@@ -28,30 +29,63 @@ const Products = () => {
 
 
   return (
+    <div className="container-fluid row mt-3 home-page">
+  <div className="col-md-9">
     <div className="row dashboard">
-    
-    <div className="col-md-9 ">
-       
-      <div className="d-flex flex-wrap">
-        {products?.map((p,index) => (
-          
-            <div key={index} className="card m-2" style={{ width: "18rem" }}>
-               
+      <div className="col-md-10">
+        <div className="d-flex flex-wrap justify-content-around align-items-center">
+          {products?.map((p, index) => (
+            <div key={index} className="card m-3" style={{ width: "18rem", boxShadow: "0 6px 12px rgba(0,0,0,0.1)", borderRadius: "12px" }}>
               <div className="card-body">
-                <h2 className="card-title">{p.name}</h2>
-                <p className="card-text">{p.price}</p>
-                <p className="card-text">{p.type}</p>
-                <p className="card-text">{p.processor}</p>
-                <p className="card-text">{p.memory}</p>
-                <p className="card-text">{p.os}</p>
-
+                <h2 className="card-title" style={{ fontSize: "1.8rem", fontWeight: "bold", marginBottom: "15px" }}>{p.name}</h2>
+                <p className="card-text" style={{ fontSize: "1.4rem", marginBottom: "10px" }}>Price: ${p.price}</p>
+                <p className="card-text" style={{ fontSize: "1.2rem", marginBottom: "10px" }}>Type: {p.type}</p>
+                <p className="card-text" style={{ fontSize: "1.2rem", marginBottom: "10px" }}>Processor: {p.processor}</p>
+                <p className="card-text" style={{ fontSize: "1.2rem", marginBottom: "10px" }}>Memory: {p.memory}</p>
+                <p className="card-text" style={{ fontSize: "1.2rem", marginBottom: "10px" }}>OS: {p.os}</p>
+                <button className="btn btn-info ms-1">
+                  More Details
+                </button>
+                <button
+                  className="btn btn-dark ms-1">
+                  ADD TO CART
+                </button>
               </div>
             </div>
-          
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   </div>
+
+  <div className="col-md-3 filters text-center" style={{ textAlign: "left", fontSize: "2.1rem" }}>
+    <h4 className="mb-4" style={{  fontSize: "2.1rem" }}>Filter By Category</h4>
+    <div className="d-flex  align-items-center">
+      <input type="checkbox" id="android" style={{ fontSize: "5rem" }} />
+      <label htmlFor="android" style={{ fontSize: "2rem", marginBottom: "10px" }}>Android</label>
+    </div>
+
+    {/* Add more filter options as needed */}
+
+    {/* Price filter */}
+    <div className="d-flex flex-column mt-3">
+      <button
+        className="btn btn-danger"
+        onClick={() => window.location.reload()}
+        style={{ fontSize: "1.6rem" }}
+      >
+        RESET FILTERS
+      </button>
+    </div>
+  </div>
+</div>
+
+
+
+
+    
+ 
+
   )
 }
 
